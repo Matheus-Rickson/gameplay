9f216f9dfaff7f2091f09b0a0cc410db0bd76b48
Matheus-Rickson <Contato.rickson@outlook.com>
2021-06-20 23:48:44 -0300

    v2

---
+++ Src/global/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    background: "#4a1149",
+    heading: "#DDE3F0",
+    primary: "#E51C44",
+    line: "#991f36",
+  },
+};
--- Src/screens/SignIn/index.tsx
+++ Src/screens/SignIn/index.tsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, StatusBar } from "react-native";
+import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
+import { ButtonIcon } from "../../components/buttonIcon";
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> Hello World </Text>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente {`\n`}
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.75} />
+      </View>
     </View>
   );
 }
